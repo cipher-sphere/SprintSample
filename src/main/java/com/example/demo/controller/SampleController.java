046346fe89ba9ddf8d45a285b8c09bc92dceae50
itmeshruti <shrutisaxena42003@gmail.com>
2026-04-24 15:05:01 +0530

    From Shruti

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/person1")
+    public String getMessage() {
+        return "Hi I am from machine of person 1";
+    }
+    @GetMapping("/person2")
+    public String getMessage1() {
+        return "Hi I am from machine of person 2";
+    }
+}
