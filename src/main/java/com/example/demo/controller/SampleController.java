e8e70bf91f46022473dc56ebd89f7923c74466ae
garima <garima.7.2003@gmail.com>
2026-04-24 15:16:59 +0530

    garima

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -6,18 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-<<<<<<< HEAD
-    @GetMapping("/Shivam")
-    public String getMessage() {
-        return "Hi I am from machine of Shivam";
-=======
-    @GetMapping("/person1")
-    public String getMessage() {
-        return "Hi I am from machine of person 1";
-    }
-    @GetMapping("/person2")
+
+    
+    @GetMapping("/garima")
     public String getMessage1() {
-        return "Hi I am from machine of person 2";
->>>>>>> origin/shruti
+        return "Hi I am from machine garima ";
+
     }
+    
 }
