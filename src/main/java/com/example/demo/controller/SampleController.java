bf6e1afe2f6b1fc43906de5ebd8a309b5c33d52c
cipher-sphere <shivam0hapur@gmail.com>
2026-04-24 14:56:12 +0530

    controller

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
 
+    @GetMapping("/person1")
+    public String getMessage() {
+        return "Hi I am from machine of person 1";
+    }
 }
